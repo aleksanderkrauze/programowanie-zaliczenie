3ff9c0ca6b840e8f06b50a9d1975d18c793ec84f
Aleksander Krauze <alkoszczak@gmail.com>
2022-03-19 08:37:01 +0100

    Wrapped functions in namespace `plotter`

--- include/plotter.h
+++ include/plotter.h
@@ -6,23 +6,25 @@
 
 #include "person.h"
 
-///////////////////////////////////////////////
-// Rysowanie stanu miasta (położenia wszystkich osób) w danym kroku czasowym
-// (timeStep). Zapisywanie rysunku do pliku o nazwie plots/frame_xxxx.png
-// gdzie xxxx oznacza numer klatki (kolejnego kroku czasowego)
-///////////////////////////////////////////////
-void plot(std::vector<Person>&, std::uint32_t);
+namespace plotter {
+	///////////////////////////////////////////////
+	// Rysowanie stanu miasta (położenia wszystkich osób) w danym kroku czasowym
+	// (timeStep). Zapisywanie rysunku do pliku o nazwie plots/frame_xxxx.png
+	// gdzie xxxx oznacza numer klatki (kolejnego kroku czasowego)
+	///////////////////////////////////////////////
+	void plot(std::vector<Person>&, std::uint32_t);
 
-///////////////////////////////////////////////
-// Tworzenie animowanego pliku w formacie gif ze wszystkich dostępnych
-// stop klatek zapisanych w plikach frame_xxxx.png
-// Użycie polecenia system i programu convert (dostępne na komputerach OKWF).
-///////////////////////////////////////////////
-void makeAnimation();
+	///////////////////////////////////////////////
+	// Tworzenie animowanego pliku w formacie gif ze wszystkich dostępnych
+	// stop klatek zapisanych w plikach frame_xxxx.png
+	// Użycie polecenia system i programu convert (dostępne na komputerach OKWF).
+	///////////////////////////////////////////////
+	void makeAnimation();
 
-///////////////////////////////////////////////
-// Usuwanie starych rysunków i animacji
-///////////////////////////////////////////////
-void clean(); 
+	///////////////////////////////////////////////
+	// Usuwanie starych rysunków i animacji
+	///////////////////////////////////////////////
+	void clean();
+}
 
 #endif // PLOTTER_H
--- src/plotter.cpp
+++ src/plotter.cpp
@@ -9,44 +9,46 @@
 
 namespace plt = matplotlibcpp;
 
-void plot(std::vector<Person>& objects, std::uint32_t timeStep) {
-  // double xLimHist = xx;
-  // double yLimHist = yy;
-  
-  // plt::xlim(0., xLimHist);
-  // plt::ylim(0., yLimHist);  
-  // plt::xlabel("x");
-  // plt::ylabel("y");
-
-  // double radius;
-  // double circleArea;
-
-  // std::vector<double> x, y;
-  // double radiusToPixel = 200;
-	// for(auto& it: objects){
-		// radius = it.getRadius()*radiusToPixel;
-		// circleArea = M_PI*radius*radius;    
-		// x.push_back(it.getX());
-		// y.push_back(it.getY());
-		// plt::scatter(x,y,circleArea, {{"color",it.getColor()}}); 
-		// x.clear();
-		// y.clear();
-	// }
-
-  // std::ostringstream ostr;
-  // int frameNumberWidth = 4;
-  // ostr<< "plots/frame_"<< std::setfill('0') << std::setw(frameNumberWidth) <<timeStep<<".png";
-  // plt::save(ostr.str());
-  // plt::close();
+namespace plotter {
+	void plot(std::vector<Person>& objects, std::uint32_t timeStep) {
+	  // double xLimHist = xx;
+	  // double yLimHist = yy;
+	  
+	  // plt::xlim(0., xLimHist);
+	  // plt::ylim(0., yLimHist);  
+	  // plt::xlabel("x");
+	  // plt::ylabel("y");
+	
+	  // double radius;
+	  // double circleArea;
+	
+	  // std::vector<double> x, y;
+	  // double radiusToPixel = 200;
+		// for(auto& it: objects){
+			// radius = it.getRadius()*radiusToPixel;
+			// circleArea = M_PI*radius*radius;    
+			// x.push_back(it.getX());
+			// y.push_back(it.getY());
+			// plt::scatter(x,y,circleArea, {{"color",it.getColor()}}); 
+			// x.clear();
+			// y.clear();
+		// }
+	
+	  // std::ostringstream ostr;
+	  // int frameNumberWidth = 4;
+	  // ostr<< "plots/frame_"<< std::setfill('0') << std::setw(frameNumberWidth) <<timeStep<<".png";
+	  // plt::save(ostr.str());
+	  // plt::close();
+	}
+	
+	void makeAnimation() {
+	  std::cout<<"Plotter: making animation"<<std::endl;
+	  std::string command = "cd plots; convert frame_*.png animation.gif";
+	  system(command.c_str());
+	  std::cout<<"Done."<<std::endl;
+	}
+	
+	void clean() {
+	  system("cd plots; rm -f animation.gif; rm -f frame*.png");
+	} 
 }
-
-void makeAnimation() {
-  std::cout<<"Plotter: making animation"<<std::endl;
-  std::string command = "cd plots; convert frame_*.png animation.gif";
-  system(command.c_str());
-  std::cout<<"Done."<<std::endl;
-}
-
-void clean() {
-  system("cd plots; rm -f animation.gif; rm -f frame*.png");
-} 
