0c6478fcef6878b61a57f0930de008754683a526
Aleksander Krauze <alkoszczak@gmail.com>
2022-03-19 22:20:09 +0100

    Written a base Person class and it's getters and setters member functions

--- include/person.h
+++ include/person.h
@@ -1,6 +1,32 @@
 #ifndef PERSON_H
 #define PERSON_H
 
-class Person {};
+#include "plotter.h"
+
+enum class InfectionStatus {GREEN, RED, BLUE};
+
+class Person: public Drawable {
+private:
+	double x, y, vx, vy, radius, time_of_infection;
+	InfectionStatus infection_status;
+public:
+	Person(double x, double y, double vx, double vy, double r, InfectionStatus status);
+	~Person() override = default;
+	double member_x() const;
+	void member_x(double);
+	double member_y() const;
+	void member_y(double);
+	double member_vx() const;
+	void member_vx(double);
+	double member_vy() const;
+	void member_vy(double);
+	double member_radius() const;
+	void member_radius(double);
+	double member_time_of_infection() const;
+	void member_time_of_infection(double);
+	InfectionStatus member_infection_status() const;
+	void member_infection_status(InfectionStatus);
+	void draw() const override;
+};
 
 #endif // PERSON_H
--- src/person.cpp
+++ src/person.cpp
@@ -1,1 +1,66 @@
 #include "person.h"
+
+Person::Person(double x, double y, double vx, double vy, double r, InfectionStatus status):
+	x {x}, y {y}, vx {vx}, vy {vy}, radius {r}, infection_status {status}, time_of_infection {0.0}
+{
+}
+
+double Person::member_x() const {
+	return x;
+}
+
+void Person::member_x(double x) {
+	x = x;
+}
+
+double Person::member_y() const {
+	return y;
+}
+
+void Person::member_y(double y) {
+	y = y;
+}
+
+double Person::member_vx() const {
+	return vx;
+}
+
+void Person::member_vx(double vx) {
+	vx = vx;
+}
+
+double Person::member_vy() const {
+	return vy;
+}
+
+void Person::member_vy(double vy) {
+	vy = vy;
+}
+
+double Person::member_radius() const {
+	return radius;
+}
+
+void Person::member_radius(double r) {
+	radius = r;
+}
+
+double Person::member_time_of_infection() const {
+	return time_of_infection;
+}
+
+void Person::member_time_of_infection(double time) {
+	time_of_infection = time;
+}
+
+InfectionStatus Person::member_infection_status() const {
+	return infection_status;
+}
+
+void Person::member_infection_status(InfectionStatus status) {
+	infection_status = status;
+}
+
+void Person::draw() const {
+	// TODO: write drawing function
+}
