c3441b836532600d71f0cf85ae3b18ac57b0d216
Aleksander Krauze <alkoszczak@gmail.com>
2022-04-10 22:35:57 +0200

    Added `noexcept` keyword to all Vector2d's member functions

--- include/vector2d.h
+++ include/vector2d.h
@@ -5,9 +5,9 @@
 
 class Vector2d {
 public:
-  Vector2d();
-  Vector2d(const double, const double);
-  explicit Vector2d(const std::tuple<double, double>);
+  Vector2d() noexcept;
+  Vector2d(const double, const double) noexcept;
+  explicit Vector2d(const std::tuple<double, double>) noexcept;
 
   Vector2d(const Vector2d&) = default;
   Vector2d& operator=(const Vector2d&) = default;
@@ -15,31 +15,31 @@ public:
   Vector2d& operator=(Vector2d&&) = default;
   ~Vector2d() = default;
 
-  double x() const;
-  void x(const double);
-  double y() const;
-  void y(const double);
-  std::tuple<double, double> tuple() const;
-  void tuple(const std::tuple<double, double>);
-  double length() const;
-  static double distance(const Vector2d&, const Vector2d&);
+  double x() const noexcept;
+  void x(const double) noexcept;
+  double y() const noexcept;
+  void y(const double) noexcept;
+  std::tuple<double, double> tuple() const noexcept;
+  void tuple(const std::tuple<double, double>) noexcept;
+  double length() const noexcept;
+  static double distance(const Vector2d&, const Vector2d&) noexcept;
 
-  bool operator==(const Vector2d&) const;
-  bool operator!=(const Vector2d&) const;
-  Vector2d operator+() const;
-  Vector2d operator-() const;
-  Vector2d operator+(const Vector2d&) const;
-  Vector2d operator-(const Vector2d&) const;
-  Vector2d operator*(const double) const;        // scalar multiplication
-  double operator*(const Vector2d&) const; // dot product
-  Vector2d& operator+=(const Vector2d&);
-  Vector2d& operator-=(const Vector2d&);
-  Vector2d& operator*=(const double); // scalar multiplication
+  bool operator==(const Vector2d&) const noexcept;
+  bool operator!=(const Vector2d&) const noexcept;
+  Vector2d operator+() const noexcept;
+  Vector2d operator-() const noexcept;
+  Vector2d operator+(const Vector2d&) const noexcept;
+  Vector2d operator-(const Vector2d&) const noexcept;
+  Vector2d operator*(const double) const noexcept;  // scalar multiplication
+  double operator*(const Vector2d&) const noexcept; // dot product
+  Vector2d& operator+=(const Vector2d&) noexcept;
+  Vector2d& operator-=(const Vector2d&) noexcept;
+  Vector2d& operator*=(const double) noexcept; // scalar multiplication
 
 private:
   double m_x, m_y;
 };
 
-Vector2d operator*(const double, const Vector2d&);
+Vector2d operator*(const double, const Vector2d&) noexcept;
 
 #endif // VECTOR_2D_H
--- src/vector2d.cpp
+++ src/vector2d.cpp
@@ -3,96 +3,100 @@
 
 #include "vector2d.h"
 
-Vector2d::Vector2d() : m_x{0.0}, m_y{0.0} {}
+Vector2d::Vector2d() noexcept : m_x{0.0}, m_y{0.0} {}
 
-Vector2d::Vector2d(const double x, const double y) : m_x{x}, m_y{y} {}
+Vector2d::Vector2d(const double x, const double y) noexcept : m_x{x}, m_y{y} {}
 
-Vector2d::Vector2d(const std::tuple<double, double> coordinates)
+Vector2d::Vector2d(const std::tuple<double, double> coordinates) noexcept
     : m_x{std::get<0>(coordinates)}, m_y{std::get<1>(coordinates)} {}
 
-double Vector2d::x() const { return this->m_x; }
+double Vector2d::x() const noexcept { return this->m_x; }
 
-void Vector2d::x(const double x) { this->m_x = x; }
+void Vector2d::x(const double x) noexcept { this->m_x = x; }
 
-double Vector2d::y() const { return this->m_y; }
+double Vector2d::y() const noexcept { return this->m_y; }
 
-void Vector2d::y(const double y) { this->m_y = y; }
+void Vector2d::y(const double y) noexcept { this->m_y = y; }
 
-std::tuple<double, double> Vector2d::tuple() const {
+std::tuple<double, double> Vector2d::tuple() const noexcept {
   return std::make_tuple(this->m_x, this->m_y);
 }
 
-void Vector2d::tuple(const std::tuple<double, double> coordinates) {
+void Vector2d::tuple(const std::tuple<double, double> coordinates) noexcept {
   const auto [x, y] = coordinates;
   this->m_x = x;
   this->m_y = y;
 }
 
-double Vector2d::length() const {
+double Vector2d::length() const noexcept {
   const auto square = [](auto x) { return x * x; };
   return std::sqrt(square(this->m_x) + square(this->m_y));
 }
 
-double Vector2d::distance(const Vector2d& lhs, const Vector2d& rhs) {
+double Vector2d::distance(const Vector2d& lhs, const Vector2d& rhs) noexcept {
   const auto diff = lhs - rhs;
   return diff.length();
 }
 
-bool Vector2d::operator==(const Vector2d& rhs) const {
+bool Vector2d::operator==(const Vector2d& rhs) const noexcept {
   const auto epsilon = 1e-9;
   const auto distance = Vector2d::distance(*this, rhs);
 
   return distance < epsilon;
 }
 
-bool Vector2d::operator!=(const Vector2d& rhs) const { return !(*this == rhs); }
+bool Vector2d::operator!=(const Vector2d& rhs) const noexcept {
+  return !(*this == rhs);
+}
 
-Vector2d Vector2d::operator+() const { return *this; }
+Vector2d Vector2d::operator+() const noexcept { return *this; }
 
-Vector2d Vector2d::operator-() const {
+Vector2d Vector2d::operator-() const noexcept {
   const Vector2d zero{};
   return zero - *this;
 }
 
-Vector2d Vector2d::operator+(const Vector2d& rhs) const {
+Vector2d Vector2d::operator+(const Vector2d& rhs) const noexcept {
   Vector2d v{*this};
   v += rhs;
   return v;
 }
 
-Vector2d Vector2d::operator-(const Vector2d& rhs) const {
+Vector2d Vector2d::operator-(const Vector2d& rhs) const noexcept {
   Vector2d v{*this};
   v -= rhs;
   return v;
 }
 
-Vector2d Vector2d::operator*(const double scalar) const {
+Vector2d Vector2d::operator*(const double scalar) const noexcept {
   Vector2d v{*this};
   v *= scalar;
   return v;
 }
 
-Vector2d operator*(const double scalar, const Vector2d& rhs) { return rhs * scalar; }
+Vector2d operator*(const double scalar, const Vector2d& rhs) noexcept {
+  return rhs * scalar;
+}
 
-double Vector2d::operator*(const Vector2d& rhs) const {
+double Vector2d::operator*(const Vector2d& rhs) const noexcept {
   return (this->m_x * rhs.m_x) + (this->m_y * rhs.m_y);
 }
 
-Vector2d& Vector2d::operator+=(const Vector2d& rhs) {
+Vector2d& Vector2d::operator+=(const Vector2d& rhs) noexcept {
   this->m_x += rhs.m_x;
   this->m_y += rhs.m_y;
 
   return *this;
 }
 
-Vector2d& Vector2d::operator-=(const Vector2d& rhs) {
+Vector2d& Vector2d::operator-=(const Vector2d& rhs) noexcept {
   this->m_x -= rhs.m_x;
   this->m_y -= rhs.m_y;
 
   return *this;
 }
 
-Vector2d& Vector2d::operator*=(const double scalar) {
+Vector2d& Vector2d::operator*=(const double scalar) noexcept {
   this->m_x *= scalar;
   this->m_y *= scalar;
 
