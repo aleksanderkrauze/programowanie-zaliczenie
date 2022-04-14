e9a0ab76a1827ee0b7ddb56ef8149fb4787e5de9
Aleksander Krauze <alkoszczak@gmail.com>
2022-04-14 16:33:16 +0200

    Added Debug and Release profiles to Cmake

--- build.sh
+++ build.sh
@@ -3,5 +3,16 @@
 [ ! -d build ] && mkdir build
 [ ! -d plots ] && mkdir plots
 cd build
-cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 ..
+
+case "$1" in
+  "" | "d" | "debug")
+		echo Building in debug mode
+		cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_BUILD_TYPE=Debug ..
+    break ;;
+  "r" | "release")
+		echo Building in release mode
+		cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_BUILD_TYPE=Release ..
+    break ;;
+esac
+
 cmake --build .
