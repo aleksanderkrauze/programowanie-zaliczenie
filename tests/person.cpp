c9a6e82e96253b9f586c8a7ddaa1f446012c84b3
Aleksander Krauze <alkoszczak@gmail.com>
2022-04-20 00:36:25 +0200

    Made full test suite for Person::move()

--- tests/person.cpp
+++ tests/person.cpp
@@ -1,3 +1,5 @@
+#include <chrono>
+#include <random>
 #include <utility>
 
 #include <gtest/gtest.h>
@@ -218,11 +220,70 @@ TEST(Person, move_single_bounce_bottom) {
 }
 
 TEST(Person, move_double_bounce_top_right) {
-  test_move_one_ten({8, 8}, {2.5, 5}, {9.5, 7}); // this works
-  test_move_one_ten({9, 8}, {3, 3}, {8, 9});     // and this doesn't
+  // top first
+  test_move_one_ten({8, 8}, {2.5, 5}, {9.5, 7});
+  test_move_one_ten({9, 9}, {2, 1 + 2.0 / 3.0}, {9 + 1.0 / 3.0, 6});
+
+  // rigth first
+  test_move_one_ten({9, 8}, {3, 3}, {8, 9});
+  test_move_one_ten({10, 9}, {5, 1.25}, {5, 9.75});
 }
 
-TEST(Person, move_from_side_to_side) {
+TEST(Person, move_double_bounce_top_left) {
+  // top first
+  test_move_one_ten({1, 10}, {-1.5, 3}, {0.5, 7});
+  test_move_one_ten({3, 8}, {-5, 5}, {2, 7});
+
+  // left first
+  test_move_one_ten({1, 9}, {-4, 2}, {3, 9});
+  test_move_one_ten({3, 8}, {-9, 3}, {6, 9});
+}
+
+TEST(Person, move_double_bounce_bottom_left) {
+  // bottom first
+  test_move_one_ten({2, 1}, {-3, -6}, {1, 5});
+  test_move_one_ten({5, 1}, {-7.5, -3}, {2.5, 2});
+
+  // left first
+  test_move_one_ten({2, 1}, {-4, -4}, {4, 2});
+  test_move_one_ten({1, 1}, {-3, -1}, {2, 1.0 / 3.0});
+}
+
+TEST(Person, move_double_bounce_bottom_right) {
+  // bottom first
+  test_move_one_ten({8, 1}, {2.5, -2.5}, {9.5, 1.5});
+  test_move_one_ten({9, 1}, {2, -4}, {9, 3});
+
+  // right first
+  test_move_one_ten({10, 2}, {1.5, -3}, {8.5, 1});
+  test_move_one_ten({8, 2}, {5, -3}, {1.0 / 3.0, 7});
+}
+
+TEST(Person, move_corner_top_right) {
+  test_move_one_ten({8, 8}, {3, 3}, {9, 9});
+  test_move_one_ten({9, 8}, {2, 4}, {9, 8});
+  test_move_one_ten({7, 9}, {9, 3}, {4, 8});
+}
+
+TEST(Person, move_corner_top_left) {
+  test_move_one_ten({1, 9}, {-3, 3}, {2, 8});
+  test_move_one_ten({1, 5}, {-1.2, 6}, {0.2, 9});
+  test_move_one_ten({2, 9}, {-4, 2}, {2, 9});
+}
+
+TEST(Person, move_corner_bottom_left) {
+  test_move_one_ten({1, 1}, {-2, -2}, {1, 1});
+  test_move_one_ten({1, 2}, {-1.5, -3}, {0.5, 1});
+  test_move_one_ten({4, 1}, {-6, -1.5}, {2, 0.5});
+}
+
+TEST(Person, move_corner_bottom_right) {
+  test_move_one_ten({7, 3}, {4, -4}, {9, 1});
+  test_move_one_ten({7, 2}, {4.5, -3}, {8.5, 1});
+  test_move_one_ten({8, 4}, {3, -6}, {9, 2});
+}
+
+TEST(Person, move_from_edge_to_edge) {
   Person p{{0, 5}, {10, 1}, 1, Person::InfectionStatus::GREEN};
 
   p.move(1, 10);
@@ -231,6 +292,73 @@ TEST(Person, move_from_side_to_side) {
   ASSERT_EQ(p.position(), Vector2d(0, 7));
   p.move(1, 10);
   ASSERT_EQ(p.position(), Vector2d(10, 8));
+}
+
+TEST(Person, move_from_corner_to_corner) {
+  test_move_one_ten({0, 0}, {11, 11}, {9, 9});
+  test_move_one_ten({10, 10}, {-20, -20}, {10, 10});
+  test_move_one_ten({10, 0}, {-15, 15}, {5, 5});
+  test_move_one_ten({0, 10}, {25, -25}, {5, 5});
+}
+
+TEST(Person, move_reverse_fuzzy_testing) {
+  const auto N = 1000;
+  const auto dt = 1.0;
+  const auto city_size = 10.0;
+
+  const unsigned seed =
+    std::chrono::system_clock::now().time_since_epoch().count();
+  std::default_random_engine generator{seed};
+  std::uniform_real_distribution<double> position_distribution{0, city_size};
+  std::uniform_real_distribution<double> velocity_distribution{0,
+                                                               city_size / 2.0};
+
+  const auto get_position = [&generator, &position_distribution]() {
+    const auto x = position_distribution(generator);
+    const auto y = position_distribution(generator);
+
+    return Vector2d{x, y};
+  };
+
+  const auto get_velocity = [&generator, &velocity_distribution]() {
+    const auto x = velocity_distribution(generator);
+    const auto y = velocity_distribution(generator);
+
+    return Vector2d{x, y};
+  };
+
+  Vector2d starting_position, starting_velocity;
+
+  for (auto i = 0; i < N; ++i) {
+    try {
+      starting_position = get_position();
+      starting_velocity = get_velocity();
+
+      Person p1{starting_position, starting_velocity, 1.0,
+                Person::InfectionStatus::GREEN};
+      p1.move(dt, city_size);
+
+      const auto final_position = p1.position();
+      const auto final_velocity = p1.velocity();
+
+      Person p2{final_position, -final_velocity, 1.0,
+                Person::InfectionStatus::GREEN};
+      p2.move(dt, city_size);
+
+      ASSERT_EQ(std::make_pair(p2.position(), p2.velocity()),
+                std::make_pair(starting_position, -starting_velocity))
+        << "Reverse fuzzy test failed for following inputs:" << std::endl
+        << "\tdt = " << dt << " city_size = " << city_size << std::endl
+        << "\tstarting_position = " << starting_position << std::endl
+        << "\tstarting_velocity = " << starting_velocity << std::endl;
+    } catch (std::runtime_error& e) {
+      FAIL() << "Caught runtime error: " << e.what() << std::endl
+             << "Reverse fuzzy test failed for following inputs:" << std::endl
+             << "\tdt = " << dt << " city_size = " << city_size << std::endl
+             << "\tstarting_position = " << starting_position << std::endl
+             << "\tstarting_velocity = " << starting_velocity << std::endl;
+    }
+  }
 }
 
 TEST(Person, move_known_past_bugs) {
